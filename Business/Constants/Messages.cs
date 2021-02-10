b8bf59c62110e179d6d3080778eba573300ccc04
Doğukan Yılmaz <dogu_codeM@hotmail.com>
2021-02-11 00:04:18 +0300

    10.02.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+
+        public static string ProductNameInvalid = "Ürün ismi geçersiz, en az 2 karakter kullanın";
+
+        public static string MaintenanceTime = "Sistem bakımda";
+
+        public static string ProductListed = "Ürünler listelendi";
+    }
+}
